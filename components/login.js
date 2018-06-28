3993bb457e9f6d9f9f4aaa6e9b829321be4b1fca
Ryan <bangerterryan350@gmail.com>
2018-06-28 09:30:23 -0600

    new
    new

---
+++ components/login.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Image, KeyboardAvoidingView, ImageBackground} from 'react-native'
+import Loginform from './Loginform';
+export default class Login extends Component {
+  render() {
+    return (
+      <KeyboardAvoidingView behavior='padding' style={styles.container}>
+        <ImageBackground 
+        source={require('../assets/images/background.jpg')}
+        style ={styles.mainwallpaper}>
+          <View style={styles.logoContainer}>
+              <Image 
+              style={styles.logo}
+              source={require('../assets/images/logo3.png')}
+              />
+          </View>
+          <View style={styles.formContainer}>
+          <Loginform/>
+          </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: 'white'
+  },
+  mainwallpaper:{
+    flex: 1,
+    width: undefined,
+    height: undefined,
+    backgroundColor:'transparent',
+  },
+  logoContainer:{
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center'
+  },
+  logo:{
+    width: 350,
+    height: 350
+  },
+  title:{
+    color: '#ffffff',
+    marginTop: 10,
+    width: 200,
+    textAlign: 'center',
+    opacity: 0.6
+  },
+  formContainer:{
+    // backgroundColor: 'rgba(0, 43, 163, 0.73)',
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center'
+  }
+});
---
+++ navigation/AppNavigator.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { createSwitchNavigator } from 'react-navigation';
+import {
+  ActivityIndicator,
+  AsyncStorage,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+class AuthLoadingScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this._bootstrapAsync();
+  }
+
+  // Fetch the token from storage then navigate to our appropriate place
+  _bootstrapAsync = async () => {
+    const userToken = await AsyncStorage.getItem('userToken');
+
+    // This will switch to the App screen or Auth screen and this loading
+    // screen will be unmounted and thrown away.
+    this.props.navigation.navigate(userToken ? 'App' : 'Auth');
+  };
+
+  // Render any loading content that you like here
+  render() {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+        <StatusBar barStyle="default" />
+      </View>
+    );
+  }
+}
+
+import MainTabNavigator from './MainTabNavigator';
+import Login from '../components/login';
+
+export default createSwitchNavigator({
+  AuthLoading: AuthLoadingScreen,
+  Main: MainTabNavigator,
+  Auth: Login
+},
+{
+   initialRouteName: 'Auth',
+
+});
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: 'black'
+  }
+});
