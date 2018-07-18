1afaa5051001c71a6ab21d3fd7394381656f47b3
Bobby-Lishez <right2bob@msn.com>
2018-07-17 19:39:05 -0600

    moved some files

--- Client/components/MonthlyNutrients.js
+++ Client/components/MonthlyNutrients.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import {Text, View} from 'react-native'
-import { LineChart } from 'react-native-svg-charts'
+import { LineChart, Grid, XAxis, YAxis } from 'react-native-svg-charts'
 
 class MonthlyNutrients extends React.PureComponent {
     state = {
         albumen: [],
         phosphorous: [],
         potassium: []
-    }
+    };
 
+    
     render() {
+
+        const data1 = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ];
+        const data2 = [40, 20, 30, 85, -14, -14, 95, 81, 45, 63, -63, 14, 40, -10, -70 ];
+        
+        const axesSvg = { fontSize: 10, fill: 'grey' };
+        const verticalContentInset = { top: 10, bottom: 10 }
+        const xAxisHeight = 30;
+
 
         return (
             <View style={{ height: 200, padding: 20, flexDirection: 'row' }}>
             <YAxis
-                data={data}
+                data={data1}
                 style={{ marginBottom: xAxisHeight }}
                 contentInset={verticalContentInset}
                 svg={axesSvg}
@@ -22,15 +31,23 @@ class MonthlyNutrients extends React.PureComponent {
             <View style={{ flex: 1, marginLeft: 10 }}>
                 <LineChart
                     style={{ flex: 1 }}
-                    data={data}
+                    data={data1}
                     contentInset={verticalContentInset}
-                    svg={{ stroke: 'rgb(134, 65, 244)' }}
+                    svg={{ stroke: 'rgb(255, 0, 0)' }}
                 >
-                    <Grid/>
+                    {/* <Grid/> */}
+                </LineChart>
+                <LineChart
+                    style={{ flex: 1 }}
+                    data={data2}
+                    contentInset={verticalContentInset}
+                    svg={{ stroke: 'rgb(0, 255, 0)' }}
+                >
+                    {/* <Grid/> */}
                 </LineChart>
                 <XAxis
                     style={{ marginHorizontal: -10, height: xAxisHeight }}
-                    data={data}
+                    data={data1}
                     formatLabel={(value, index) => index}
                     contentInset={{ left: 10, right: 10 }}
                     svg={axesSvg}
